cb5dd34044bed86bf45631307948d939c8d3d3c2
STiago521 <santiago.pena_agu@uao.edu.co>
2025-10-29 22:47:51 -0500

    Fantasa

---
+++ My project (1)/Assets/JumpscareTrigger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class JumpscareTrigger : MonoBehaviour
+{
+    public GameObject jumpscarePanel; // asignas el panel desde el inspector
+    public float scareDuration = 2f; // segundos que dura el jumpscare
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera")) // si el jugador entra
+        {
+            StartCoroutine(ShowJumpscare());
+        }
+    }
+
+    private System.Collections.IEnumerator ShowJumpscare()
+    {
+        jumpscarePanel.SetActive(true);
+        yield return new WaitForSeconds(scareDuration);
+        jumpscarePanel.SetActive(false);
+    }
+}
+
